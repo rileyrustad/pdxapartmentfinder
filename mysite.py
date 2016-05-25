09a4604e76951a56d0300e73337270c668842e55
Riley Rustad <rileyrustad@gmail.com>
2016-05-24 20:00:18 -0700

    Implemented wtforms

--- mysite.py
+++ mysite.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, Length
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret!'
 boostrap = Bootstrap(app)
 
+class NameForm(Form):
+    id_number = StringField('What is the listing ID Number?',
+    	validators=[Required(), 
+    	Length(10, message = 'Field must be 10 digit craigslist ID Number')])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
@@ -12,8 +22,10 @@ def index():
 @app.route('/scorer', methods=['GET', 'POST'])
 def scorer():
 	id_number = None
-	if request.method == 'POST' and 'id_number' in request.form:
-		id_number = request.form['id_number']
+	form = NameForm()
+	if form.validate_on_submit():
+		id_number = form.id_number.data
+		form.id_number.data = ''
 
 	attrs = ['bed','bath','feet','dog','cat','content',
 	'date','getphotos','hasmap','housingtype','lat','long','laundry',
@@ -24,7 +36,7 @@ def scorer():
 	'laundry': 'in unit','parking':'street parking','price':'$500','smoking':'no smoking','time':'12:00',
 	'wheelchair':'no wheelchair access'}
 	my_dict = temp_dict
-	return render_template('scorer.html', id_number=id_number, attrs=attrs,my_dict=my_dict)
+	return render_template('scorer.html', id_number=id_number, attrs=attrs,my_dict=my_dict,form=form)
 
 @app.errorhandler(404)
 def not_found(e):
